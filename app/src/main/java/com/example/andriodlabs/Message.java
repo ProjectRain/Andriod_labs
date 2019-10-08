b9d177e2d924d48e6660db019767bea329e12f00
ProjectRain <nur00068@algonquinlive.com>
2019-10-08 19:20:06 -0400

    Lab 4 Finished

---
+++ app/src/main/java/com/example/andriodlabs/Message.java
@@ -0,0 +1,21 @@
+package com.example.andriodlabs;
+
+public class Message {
+
+    private String message;
+    private boolean isSend;
+
+    public Message (boolean isSend, String message){
+        this.isSend = isSend;
+        this.message = message;
+
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
